2717c72d20c0a234319d23d1c59650ca8d7e1b1c
kevin <jessicamaiaaraujo@gmail.com>
2023-02-25 14:51:30 -0300

    trabalho javascript

---
+++ exercicio1-2.js
@@ -0,0 +1,19 @@
+// leia A, B e C
+// imprima na tela se a soma de A + B é menor que C
+
+
+// let a = 2;
+
+// let b = 4;
+
+// let c = 8;
+
+function testar(numA, numB, numC){
+    let resultado = numA + numB;
+
+    if (resultado < numC){
+        return console.log("O resultado de A + B é menor que C")
+    } else {
+        return console.log("O resultado de A + B é maior ou igual a C")
+    }
+}
