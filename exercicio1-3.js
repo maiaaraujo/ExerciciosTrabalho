b62a92025164435004fc6bdc9273c54ddf8361db
kevin <jessicamaiaaraujo@gmail.com>
2023-02-25 15:32:48 -0300

    exercício 7

---
+++ exercicio1-3.js
@@ -0,0 +1,11 @@
+let soma = 10 + 50;
+console.log(soma);
+
+let subtracao = "100" - 50;
+console.log(subtracao);
+
+let multiplicação = "100" * 2;
+console.log(multiplicação);
+
+let divisao = "10" / 2;
+console.log(divisao);
