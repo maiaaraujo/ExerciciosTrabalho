a998797d660a4fc83753cab6eecfe0fba8eb7076
kevin <jessicamaiaaraujo@gmail.com>
2023-02-25 15:47:48 -0300

    exercicio1-3

--- exercicio1-3.js
+++ exercicio1-3.js
@@ -1,11 +1,32 @@
-let soma = 10 + 50;
-console.log(soma);
+//desenvolver um algoritmo que efetue a soma de todos os numeros impares que são e que se encontram no conjunto de numeros de 1 ate 500
 
-let subtracao = "100" - 50;
-console.log(subtracao);
+// ir de 1 a 500
 
-let multiplicação = "100" * 2;
-console.log(multiplicação);
+// os numeros impares
 
-let divisao = "10" / 2;
-console.log(divisao);
+// os multiplos por 3 (divindindo por 3 da 0)
+
+let soma = 0;
+
+for (let i = 0; i < 500; i++){ //pq i tem que ser < e nao <= 500?
+    if((i % 2) != 0){ //os numeros impares
+        if((i % 3) == 0) // os numeros multiplos de 3
+        soma = soma + i;
+    }
+
+}
+
+alert (soma);
+
+
+// let soma = 10 + 50;
+// console.log(soma);
+
+// let subtracao = "100" - 50;
+// console.log(subtracao);
+
+// let multiplicação = "100" * 2;
+// console.log(multiplicação);
+
+// let divisao = "10" / 2;
+// console.log(divisao);
